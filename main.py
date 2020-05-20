bff1ceac252a24e25cab289cd59d6655676a0d47
Yousaf <waynestudent38@gmail.com>
2020-05-20 12:30:55 -0600

    minor refactoring

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import re
 FILENAME = "ShortStory.txt"
 
 class sentence(object):
+    def priority(self,s1, s2):
+        s1 = re.sub(r'\W+', '', s1)
+        s2 = re.sub(r'\W+', '', s2)
+        return sorted([s1, s2])[0] == s1
     def __init__(self, sentence):
         self.sentence = sentence
     def __lt__(self, other):
-        return priority(self.sentence,other.sentence) == 1
+        return self.priority(self.sentence,other.sentence) == 1
     def __gt__(self,other):
-        return priority(self.sentence,other.sentence) == 0
+        return self.priority(self.sentence,other.sentence) == 0
 
 def getSentences(fileName):
     with open(fileName, "r") as file:
@@ -18,11 +22,6 @@ def getSentences(fileName):
     for i in range(len(result)):
         result[i] = result[i][max([(len(v),j) for (j,v) in (enumerate(result[i]))])[1]]
     return result
-
-def priority(s1,s2):
-    s1 = re.sub(r'\W+', '', s1)
-    s2 = re.sub(r'\W+', '', s2)
-    return sorted([s1,s2])[0]==s1
 
 def driver(fileName):
     parsedSentence = getSentences(fileName)
